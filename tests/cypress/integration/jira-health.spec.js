0710400e0da76c308c60e8dbf3335b3134a9337a
branden.cobb <cobb_branden@bah.com>
2021-02-08 19:48:25 +0000

    increase cypress wait

--- tests/cypress/integration/jira-health.spec.js
+++ tests/cypress/integration/jira-health.spec.js
@@ -2,7 +2,7 @@
 describe('Basic Jira', function() {
   it('Check Jira is accessible', function() {
   cy.visit(Cypress.env('jira_url'))
-  cy.wait(2000);
+  cy.wait(5000);
   cy.contains('Jira setup') 
   })
 })
